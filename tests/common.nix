958e56da3c867f324938ab7245c080095a721488
Stefan Kober <stefan.kober@cyberus-technology.de>
2025-04-03 14:04:44 +0200

    extract common parts in file
    add sleep of 30 seconds before migration. Otherwise, the live migration
    sometimes takes forever (in the actual migrating part). unclear why
    currently.

---
+++ tests/common.nix
@@ -0,0 +1,210 @@
+{ libvirt-src }:
+{ pkgs, ... }:
+let
+  image = pkgs.fetchurl {
+    url = "https://download.cirros-cloud.net/0.6.2/cirros-0.6.2-x86_64-disk.img";
+    hash = "sha256-B+RKc+VMlNmIAoUVQDwe12IFXgG4OnZ+3zwrOH94zgA=";
+  };
+
+  image_raw = pkgs.runCommand "image_raw" { } ''
+    ${pkgs.qemu-utils}/bin/qemu-img convert -O raw ${image} $out
+  '';
+    # Network interface definition for later usage:
+    # <interface type='ethernet'>
+    #   <target dev='vnet0'/>
+    #   <model type='virtio'/>
+    #   <driver queues='1'/>
+    # </interface>
+  virsh_ch_xml = ''
+      <domain type='kvm' id='21050'>
+      <name>cirros</name>
+      <uuid>4eb6319a-4302-4407-9a56-802fc7e6a422</uuid>
+      <memory unit='KiB'>262144</memory>
+      <currentMemory unit='KiB'>262144</currentMemory>
+      <vcpu placement='static'>1</vcpu>
+      <os>
+        <type arch='x86_64'>hvm</type>
+        <kernel>/etc/hypervisor-fw</kernel>
+        <boot dev='hd'/>
+      </os>
+      <clock offset='utc'/>
+      <on_poweroff>destroy</on_poweroff>
+      <on_reboot>restart</on_reboot>
+      <on_crash>destroy</on_crash>
+      <devices>
+        <emulator>cloud-hypervisor</emulator>
+        <disk type='file' device='disk'>
+          <source file='/var/lib/libvirt/storage-pools/nfs-share/cirros.img'/>
+          <target dev='vda' bus='virtio'/>
+        </disk>
+        <serial type='pty'>
+          <source path='/dev/pts/2'/>
+          <target port='0'/>
+        </serial>
+      </devices>
+    </domain>
+  '';
+  virsh_qemu_xml = ''
+      <domain type='kvm' id='21050'>
+      <name>cirros</name>
+      <uuid>4eb6319a-4302-4407-9a56-802fc7e6a422</uuid>
+      <memory unit='KiB'>262144</memory>
+      <currentMemory unit='KiB'>262144</currentMemory>
+      <vcpu placement='static'>1</vcpu>
+      <os>
+        <type arch='x86_64'>hvm</type>
+        <boot dev='hd'/>
+      </os>
+      <clock offset='utc'/>
+      <on_poweroff>destroy</on_poweroff>
+      <on_reboot>restart</on_reboot>
+      <on_crash>destroy</on_crash>
+      <devices>
+        <emulator>/run/current-system/sw/bin/qemu-system-x86_64</emulator>
+        <disk type='file' device='disk'>
+          <source file='/var/lib/libvirt/storage-pools/nfs-share/cirros.img'/>
+          <target dev='vda' bus='virtio'/>
+        </disk>
+        <serial type='pty'>
+          <source path='/dev/pts/2'/>
+          <target port='0'/>
+        </serial>
+      </devices>
+    </domain>
+  '';
+in
+{
+  virtualisation.libvirtd = {
+    enable = true;
+    sshProxy = false;
+    package = pkgs.libvirt.overrideAttrs (old: {
+      src = libvirt-src;
+      doInstallCheck = false;
+      doCheck = false;
+    });
+  };
+
+  systemd.services.virtchd.wantedBy = [ "multi-user.target" ];
+  systemd.sockets.virtstoraged.wantedBy = [ "sockets.target" ];
+
+  systemd.network = {
+    enable = true;
+    wait-online.enable = false;
+
+    netdevs = {
+      "10-br0" = {
+        netdevConfig = {
+          Kind = "bridge";
+          Name = "br0";
+        };
+      };
+    };
+
+    networks = {
+      # Bridge interface configuration
+      "10-br0" = {
+        matchConfig.Name = "br0";
+        networkConfig = {
+          Description = "Main Bridge";
+          DHCPServer = "yes"; # Enable DHCP server on this interface
+        };
+
+        # DHCP server settings
+        dhcpServerConfig = {
+          PoolOffset = 100;
+          PoolSize = 150;
+          EmitDNS = true;
+          DNS = [
+            "8.8.8.8"
+            "8.8.4.4"
+          ]; # DNS servers to offer
+          EmitRouter = true;
+        };
+
+        # Static IP configuration for the bridge itself
+        address = [
+          "192.168.1.1/24"
+        ];
+      };
+      "10-vnet0" = {
+        matchConfig.Name = "vnet0";
+        networkConfig.Bridge = "br0";
+      };
+    };
+  };
+
+  virtualisation.libvirtd.extraConfig = ''
+    listen_tls = 0
+    listen_tcp = 1
+    auth_tcp = "none"
+  '';
+
+  networking = {
+    useDHCP = false;
+    networkmanager.enable = false;
+    useNetworkd = true;
+    firewall.enable = false;
+  };
+
+  services.getty.autologinUser = "root";
+
+  services.openssh = {
+    enable = true;
+    settings = {
+      PermitRootLogin = "yes";
+      PermitEmptyPasswords = "yes";
+    };
+  };
+
+  security.pam.services.sshd.allowNullPassword = true;
+
+  environment.systemPackages = [
+    pkgs.cloud-hypervisor
+    pkgs.qemu_kvm
+    pkgs.bridge-utils
+  ];
+
+  systemd.tmpfiles.settings =
+    let
+      chv-firmware = pkgs.fetchurl {
+        url = "https://github.com/cloud-hypervisor/rust-hypervisor-firmware/releases/download/0.5.0/hypervisor-fw";
+        hash = "sha256-Sgoel3No9rFdIZiiFr3t+aNQv15a4H4p5pU3PsFq2Vg=";
+      };
+    in
+    {
+      "10-chv" = {
+        "/etc/hypervisor-fw" = {
+          "L+" = {
+            argument = "${chv-firmware}";
+          };
+        };
+        "/etc/cirros.img" = {
+          "C+" = {
+            argument = "${image_raw}";
+          };
+        };
+        "/etc/cirros-chv.xml" = {
+          "C+" = {
+            argument = "${pkgs.writeText "cirros.xml" virsh_ch_xml}";
+          };
+        };
+        "/etc/cirros-qemu.xml" = {
+          "C+" = {
+            argument = "${pkgs.writeText "cirros.xml" virsh_qemu_xml}";
+          };
+        };
+        "/var/log/libvirt/" = {
+          D = {
+            mode = "0755";
+            user = "root";
+          };
+        };
+        "/var/log/libvirt/ch" = {
+          D = {
+            mode = "0755";
+            user = "root";
+          };
+        };
+      };
+    };
+}
--- tests/default.nix
+++ tests/default.nix
@@ -2,169 +2,6 @@
   pkgs,
   libvirt-src,
 }:
-let
-  virsh_ch_xml = ''
-      <domain type='kvm' id='21050'>
-      <name>cirros</name>
-      <uuid>4eb6319a-4302-4407-9a56-802fc7e6a422</uuid>
-      <memory unit='KiB'>262144</memory>
-      <currentMemory unit='KiB'>262144</currentMemory>
-      <vcpu placement='static'>1</vcpu>
-      <os>
-        <type arch='x86_64'>hvm</type>
-        <kernel>/etc/hypervisor-fw</kernel>
-        <boot dev='hd'/>
-      </os>
-      <clock offset='utc'/>
-      <on_poweroff>destroy</on_poweroff>
-      <on_reboot>restart</on_reboot>
-      <on_crash>destroy</on_crash>
-      <devices>
-        <emulator>cloud-hypervisor</emulator>
-        <disk type='file' device='disk'>
-          <source file='/var/lib/libvirt/storage-pools/nfs-share/cirros.img'/>
-          <target dev='vda' bus='virtio'/>
-        </disk>
-        <serial type='pty'>
-          <source path='/dev/pts/2'/>
-          <target port='0'/>
-        </serial>
-      </devices>
-    </domain>
-  '';
-  virsh_qemu_xml = ''
-      <domain type='kvm' id='21050'>
-      <name>cirros</name>
-      <uuid>4eb6319a-4302-4407-9a56-802fc7e6a422</uuid>
-      <memory unit='KiB'>262144</memory>
-      <currentMemory unit='KiB'>262144</currentMemory>
-      <vcpu placement='static'>1</vcpu>
-      <os>
-        <type arch='x86_64'>hvm</type>
-        <boot dev='hd'/>
-      </os>
-      <clock offset='utc'/>
-      <on_poweroff>destroy</on_poweroff>
-      <on_reboot>restart</on_reboot>
-      <on_crash>destroy</on_crash>
-      <devices>
-        <emulator>/run/current-system/sw/bin/qemu-system-x86_64</emulator>
-        <disk type='file' device='disk'>
-          <source file='/var/lib/libvirt/storage-pools/nfs-share/cirros.img'/>
-          <target dev='vda' bus='virtio'/>
-        </disk>
-        <serial type='pty'>
-          <source path='/dev/pts/2'/>
-          <target port='0'/>
-        </serial>
-      </devices>
-    </domain>
-  '';
-  image = pkgs.fetchurl {
-    url = "https://download.cirros-cloud.net/0.6.2/cirros-0.6.2-x86_64-disk.img";
-    hash = "sha256-B+RKc+VMlNmIAoUVQDwe12IFXgG4OnZ+3zwrOH94zgA=";
-  };
-
-  image_raw = pkgs.runCommand "image_raw" { } ''
-    ${pkgs.qemu-utils}/bin/qemu-img convert -O raw ${image} $out
-  '';
-
-  common =
-    { ... }:
-    {
-      virtualisation.libvirtd = {
-        enable = true;
-        sshProxy = false;
-        package = pkgs.libvirt.overrideAttrs (old: {
-          src = libvirt-src;
-          doInstallCheck = false;
-          doCheck = false;
-        });
-      };
-
-      systemd.services.virtchd.wantedBy = [ "multi-user.target" ];
-      systemd.sockets.virtstoraged.wantedBy = [ "sockets.target" ];
-
-      # systemd.sockets.libvirtd-tcp = {
-      #   enable = true;
-      #   wantedBy = [ "sockets.target" ];
-      # };
-
-      virtualisation.libvirtd.extraConfig = ''
-        listen_tls = 0
-        listen_tcp = 1
-        auth_tcp = "none"
-      '';
-
-      networking = {
-        useDHCP = false;
-        networkmanager.enable = false;
-        useNetworkd = true;
-        firewall.enable = false;
-      };
-
-      services.getty.autologinUser = "root";
-
-      services.openssh = {
-        enable = true;
-        settings = {
-          PermitRootLogin = "yes";
-          PermitEmptyPasswords = "yes";
-        };
-      };
-
-      security.pam.services.sshd.allowNullPassword = true;
-
-      environment.systemPackages = [
-        pkgs.cloud-hypervisor
-        pkgs.qemu_kvm
-      ];
-
-      systemd.tmpfiles.settings =
-        let
-          chv-firmware = pkgs.fetchurl {
-            url = "https://github.com/cloud-hypervisor/rust-hypervisor-firmware/releases/download/0.5.0/hypervisor-fw";
-            hash = "sha256-Sgoel3No9rFdIZiiFr3t+aNQv15a4H4p5pU3PsFq2Vg=";
-          };
-        in
-        {
-          "10-chv" = {
-            "/etc/hypervisor-fw" = {
-              "L+" = {
-                argument = "${chv-firmware}";
-              };
-            };
-            "/etc/cirros.img" = {
-              "C+" = {
-                argument = "${image_raw}";
-              };
-            };
-            "/etc/cirros-chv.xml" = {
-              "C+" = {
-                argument = "${pkgs.writeText "cirros.xml" virsh_ch_xml}";
-              };
-            };
-            "/etc/cirros-qemu.xml" = {
-              "C+" = {
-                argument = "${pkgs.writeText "cirros.xml" virsh_qemu_xml}";
-              };
-            };
-            "/var/log/libvirt/" = {
-              D = {
-                mode = "0755";
-                user = "root";
-              };
-            };
-            "/var/log/libvirt/ch" = {
-              D = {
-                mode = "0755";
-                user = "root";
-              };
-            };
-          };
-        };
-    };
-in
 pkgs.nixosTest {
   name = "Libvirt test";
 
@@ -172,7 +9,7 @@ pkgs.nixosTest {
     { ... }:
     {
       imports = [
-        common
+        (import ./common.nix { inherit libvirt-src; })
         ../modules/nfs-host.nix
       ];
 
@@ -217,7 +54,7 @@ pkgs.nixosTest {
     { ... }:
     {
       imports = [
-        common
+        (import ./common.nix { inherit libvirt-src; })
         ../modules/nfs-client.nix
       ];
 
@@ -288,7 +125,7 @@ pkgs.nixosTest {
       # Add to list of known hosts so Libvirt can connect freely via ssh afterwards
       controllerVM.succeed("ssh -o StrictHostKeyChecking=no computeVM echo")
 
-      controllerVM.succeed("virsh -c ch:///session migrate --domain cirros --desturi ch+ssh://computeVM/session --live --verbose")
+      controllerVM.succeed("sleep 30 && virsh -c ch:///session migrate --domain cirros --desturi ch+ssh://computeVM/session --live --verbose")
 
       # controllerVM.succeed("virsh -c \"qemu+tcp://controllerVM/system\" create /etc/cirros-qemu.xml")
 
